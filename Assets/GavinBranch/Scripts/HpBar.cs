e7e019ddd042f4ec48ec33066286e45088c3ac30
PoortGav <118293298+PoortGav@users.noreply.github.com>
2023-03-17 08:02:52 -0700

    Addon: I made a test screne
    In this test scene it has all the test stuff I made for for battle.

--- Assets/GavinBranch/Scripts/HpBar.cs
+++ Assets/GavinBranch/Scripts/HpBar.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class HpBar : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class HpBar : MonoBehaviour
 
     public Gradient gradient;
 
+    public TextMeshProUGUI TextOnHpBar;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,19 +24,23 @@ public class HpBar : MonoBehaviour
         
     }
 
-    public void setHealth(int health)
+    public void setHealth(float health)
     {
         slider.value = health;
 
         //updates color for health bar
         fill.color = gradient.Evaluate(slider.normalizedValue);
+
+        TextOnHpBar.text = slider.value + "/" + slider.maxValue;
     }
-    public void setMaxHealth(int health)
+    public void setMaxHealth(float health)
     {
         slider.maxValue = health;
         slider.value = health;
 
         //set color for healthbar
         fill.color = gradient.Evaluate(1);
+
+        TextOnHpBar.text = slider.value + "/" + slider.maxValue;
     }
 }
--- Assets/GavinBranch/Scripts/TestForHpBar.cs
+++ Assets/GavinBranch/Scripts/TestForHpBar.cs
@@ -6,17 +6,23 @@ using UnityEngine.UI;
 public class TestForHpBar : MonoBehaviour
 {
     //hp values
-    public int maxHp = 100;
-    public int currentHp;
+    public float maxHp = 100;
+    public float currentHp;
 
     //the script hpbar
     public HpBar healthBar;
+
+
+    public Animation hurtAnim;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         //set current to max
         currentHp = maxHp;
         healthBar.setMaxHealth(maxHp);
+
+        animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,15 +31,22 @@ public class TestForHpBar : MonoBehaviour
         //used to test damage
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            OnDamageTake(20);
+            OnDamageTake(1.5f);
         }
     }
 
-    public void OnDamageTake(int damage)
+    public void OnDamageTake(float damage)
     {
         currentHp -= damage;
 
         //updates the health bar
         healthBar.setHealth(currentHp);
+
+        animator.SetTrigger("hurt");
+
+        if(currentHp <= 0)
+        {
+            animator.SetTrigger("Die");
+        }
     }
 }
