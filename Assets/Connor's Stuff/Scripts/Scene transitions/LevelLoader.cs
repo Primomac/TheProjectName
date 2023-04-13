591492ebd0e92915166b6fbbb54f7319ce465e3c
Bfams <Bfamslike@outlook.com>
2023-04-13 07:56:10 -0700

    Gaming

--- Assets/Connor's Stuff/Scripts/Scene transitions/LevelLoader.cs
+++ Assets/Connor's Stuff/Scripts/Scene transitions/LevelLoader.cs
@@ -6,15 +6,23 @@ using UnityEngine.SceneManagement;
 public class LevelLoader : MonoBehaviour
 {
     public Animator transitionAnim;
+    private PlayerInteractions player;
 
     public float timeToLoadFor = 1f;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<PlayerInteractions>();
+    }
 
     public IEnumerator LoadLevel(string sceneToSwapTo)
     {
         //transitionAnim.SetTrigger("Start");
+        player.isTransitioning = true;
 
         yield return new WaitForSeconds(timeToLoadFor);
 
+        player.isTransitioning = false;
         SceneManager.LoadScene(sceneToSwapTo);
     }
 }
