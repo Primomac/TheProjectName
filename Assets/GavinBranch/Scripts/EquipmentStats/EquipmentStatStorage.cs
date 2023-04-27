b66dc5866f79cbed8db06763baa7429c6b77604c
PoortGav <118293298+PoortGav@users.noreply.github.com>
2023-04-27 08:59:55 -0700

    StatMenu
    I have made the stat menu not close to working though.

--- Assets/GavinBranch/Scripts/EquipmentStats/EquipmentStatStorage.cs
+++ Assets/GavinBranch/Scripts/EquipmentStats/EquipmentStatStorage.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EquipmentStatStorage : MonoBehaviour
 {
     public int[] EquipmentStatsArray;
+    public int[] baseStats;
     public GameObject EM;
     // Start is called before the first frame update
     void Start()
