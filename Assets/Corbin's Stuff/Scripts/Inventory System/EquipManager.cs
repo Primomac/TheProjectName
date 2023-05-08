ea2f574e1c15747ea1b558770f9abb56cdfbb900
IRLFemboy <115759367+IRLFemboy@users.noreply.github.com>
2023-05-08 10:27:28 -0700

    wegaerwga
    ergarg

--- Assets/Corbin's Stuff/Scripts/Inventory System/EquipManager.cs
+++ Assets/Corbin's Stuff/Scripts/Inventory System/EquipManager.cs
@@ -9,6 +9,8 @@ public class EquipManager : MonoBehaviour
 {
     public static EquipManager equipInstance;
     public List<Item> Items = new List<Item>();
+    InventoryItemController[] equipItemsArray;
+    InventoryItemController[] equipWeaponsArray;
          
     public Transform itemContent;
     public Transform weaponContent;
@@ -22,8 +24,52 @@ public class EquipManager : MonoBehaviour
     public int weaponsEquipped = 0;
 
     InventoryItemController iic;
+    InventoryItemController[] iicArray;
 
     public InventoryItemController[] equipArray;
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (SceneManager.GetActiveScene().name != "Title Scene" && SceneManager.GetActiveScene().name != "ManagerScene" && SceneManager.GetActiveScene().name != "CombatScene" && SceneManager.GetActiveScene().name != "Desert_CombatScene")
+        {
+            inventoryMenu.SetActive(true);
+            equipMenu.SetActive(true);
+            iicArray = FindObjectsOfType<InventoryItemController>();
+            Debug.Log("iic Length: " + iicArray.Length);
+            foreach (InventoryItemController iic in iicArray)
+            {
+                Debug.Log("iic Length part 2 electric boogaloo: " + iicArray.Length);
+                Debug.Log("iic found on: " + iic.gameObject.name);
+                if (iic != null)
+                {
+                    iic.SetSkills();
+                    Debug.Log("Skills set! ...Finally...");
+
+                    Item item = iic.item;
+                    if (item != null && Items.Contains(item))
+                    {
+                        iic.itemIsEquipped = true;
+                    }
+                }
+                else
+                {
+                    Debug.Log("WHYYYYYYYYY");
+                }
+            }
+            inventoryMenu.SetActive(false);
+            equipMenu.SetActive(false);
+        }
+    }
 
     private void Awake()
     {
@@ -68,8 +114,10 @@ public class EquipManager : MonoBehaviour
         ItemManager.instance.equippedItems.Remove(item);
     }
 
-    /*public void SetEquippeditems()
+    public void ListEquipItems()
     {
+        Clean();
+
         foreach(Item item in Items)
         {
             if(item.itemType == Item.ItemType.Weapon)
@@ -91,51 +139,48 @@ public class EquipManager : MonoBehaviour
                 var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
                 var equipButton = obj.transform.Find("EquipButton").GetComponent<Button>();
 
+
                 itemName.text = item.itemName;
                 itemIcon.sprite = item.icon;
                 equipButton.gameObject.SetActive(true);
             }
         }
-    }*/
+
+        SetEquippedItems();
+    }
+
+    public void SetEquippedItems()
+    {
+        equipWeaponsArray = weaponContent.GetComponentsInChildren<InventoryItemController>();
+        for (int i = 0; i < Items.Count; i++)
+        {
+            equipWeaponsArray[i].AddItem(Items[i]);
+        }
+
+        equipItemsArray = itemContent.GetComponentsInChildren<InventoryItemController>();
+        if (equipItemsArray.Length > 0)
+        {
+            for (int i = 0; i < Items.Count; i++)
+            {
+                equipItemsArray[i].AddItem(Items[i]);
+            }
+        }
+    }
+
+    public void Clean()
+    {
+        foreach (Transform item in itemContent)
+        {
+            Destroy(item.gameObject);
+        }
+        foreach(Transform item in weaponContent)
+        {
+            Destroy(item.gameObject);
+        }
+    }
+
     private void OnApplicationQuit()
     {
         //itemManager.AddItems(items);
-    }
-
-    private void OnEnable()
-    {
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    private void OnDisable()
-    {
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
-
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        if(SceneManager.GetActiveScene().name != "Title Scene" && SceneManager.GetActiveScene().name != "ManagerScene")
-        {
-            inventoryMenu.SetActive(true);
-            equipMenu.SetActive(true);
-            InventoryItemController[] iicArray = FindObjectsOfType<InventoryItemController>();
-            Debug.Log("iic Length: " + iicArray.Length);
-            foreach(InventoryItemController iic in iicArray)
-            {
-                Debug.Log("iic Length part 2 electric boogaloo: " + iicArray.Length);
-                Debug.Log("iic found on: " + iic.gameObject.name);
-                if(iic != null)
-                {
-                    iic.SetSkills();
-                    Debug.Log("Skills set! ...Finally...");
-                }
-                else
-                {
-                    Debug.Log("WHYYYYYYYYY");
-                }
-            }
-            inventoryMenu.SetActive(false);
-            equipMenu.SetActive(false);
-        }
     }
 }
